589771be19098b4b571877ee029f493497e4ebe9
Andre G <125522113+axalatar@users.noreply.github.com>
2024-12-26 17:00:52 +0000

    content buttons

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const buttons = document.querySelectorAll('button');
+const content = document.getElementById('contentHolder');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         buttons.forEach(b => b.classList.remove('active'));
         button.classList.add('active'); 
+
+        fetch("content/" + button.id + ".html").then(response => {
+            if(response.ok) {
+                return response.text();
+            }
+        }).then(response => {
+            content.innerHTML = response;
+            content.scrollIntoView({block: 'start', behavior: "smooth"})
+        })
     });
 });
